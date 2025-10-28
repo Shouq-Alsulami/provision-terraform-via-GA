c7d57ba844ec4f4010c90f991c96c2354c545298
76ilq <shoog.raja@gmail.com>
2025-10-29 02:52:34 +0300

    no node pool

--- terraform/local.tf
+++ terraform/local.tf
@@ -8,16 +8,8 @@ variable "location" {
   type        = string
   description = "Azure region"
 }
-variable "default_node_pool_name" {
-  type        = string
-  description = "Default node pool name for AKS"
-}
-
 
 locals {
-  # prefix                  = "devops2-team3-project"
-  # location                = "malaysiawest"
-  # default_node_pool_name  = "team3"
   vnet_address_space      = ["10.1.0.0/16"]
   subnet_address_prefixes = ["10.1.1.0/24"]
 }
