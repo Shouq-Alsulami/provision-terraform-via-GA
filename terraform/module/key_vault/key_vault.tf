aef29d0b413218f19502c945974aa86e4e8c62f8
Shouq-Alsulami <sulami.shouq@gmail.com>
2025-11-01 23:02:02 +0300

    kwy vault access permission

--- terraform/module/key_vault/key_vault.tf
+++ terraform/module/key_vault/key_vault.tf
@@ -17,7 +17,7 @@ resource "azurerm_key_vault" "key_vault" {
 
 
   network_acls {
-    default_action             = "Deny"
+    default_action             = "Allow"
     bypass                     = "AzureServices"
     ip_rules                   = []
   }
