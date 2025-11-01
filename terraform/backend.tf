6a4bcecd8f6cb2b5b1f75d44a9d6594c24300efe
Shouq-Alsulami <sulami.shouq@gmail.com>
2025-11-01 21:36:41 +0300

    backend config changed

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "${var.prefix}-st-rg"
-    storage_account_name = "${var.prefix}st"
-    container_name       = "tfstate"
-    key                  = "infra.tfstate"
-  }
+  backend "azurerm" {}
 }
